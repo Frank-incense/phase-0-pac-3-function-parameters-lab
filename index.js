1988d7ae7d52cee69a1728ad00ad556ae7fdbb1c
Frankincense <frankincensewelsey@gmail.com>
2025-02-24 15:26:21 +0300

    learnt about default params

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ function introductionWithLanguage(name, language) {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-function introductionWithLanguageOptional(name, language) {
-    if (language)
-    {
+function introductionWithLanguageOptional(name, language="JavaScript") {
         return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-    }
-    else{
-        return `Hi, my name is ${name} and I am learning to program in JavaScript.`;
-    }
-    
 }
